390f49f4f7611c6644fc04cebbfeb06f291fb20b
hanjiatong <hanjiatong@didichuxing.com>
2018-06-02 18:42:21 +0800

    environment

--- environment.py
+++ environment.py
@@ -8,6 +8,7 @@ class happy(object):
         self.H = H
         self.W = W
         self.max_num = max_num
+        self.points = 0
         self.reset(seed)
 
 
@@ -17,7 +18,8 @@ class happy(object):
         for i in range(1, self.max_num+1):
             weight = self.max_num-i+1
             self.sample_pool = np.concatenate((self.sample_pool, np.ones((weight, self.H, self.W))*i), axis=0)
-        self.map = np.random.choice(self.sample_pool.reshape(-1), self.W*self.H).reshape(self.W, self.H)
+        self.sample_pool = self.sample_pool.reshape(-1)
+        self.map = np.random.choice(self.sample_pool, self.W*self.H).reshape(self.W, self.H)
         self.is_visited = np.zeros((self.H, self.W), dtype=bool)
         print 'initialization succeeded! '
         print self.map, '\n'
@@ -44,7 +46,13 @@ class happy(object):
                     continue
                 index_list = []
                 self.dsp(i, j, self.map[i, j], index_list)
-                print self.map[i, j], index_list
+
+                # if find the target
+                if len(index_list) >= 3:
+                    print 'target node: ', (i, j), ' target number: ', self.map[i, j], 'target area: ', len(index_list)
+                    column_dict = self.singleupdate(index_list)
+                    self.is_visited = np.zeros((self.H, self.W), dtype=bool)
+                    print self.map
 
 
     def singleupdate(self, index_list, command_index=None):
@@ -57,20 +65,42 @@ class happy(object):
         for node in index_list:
             column_dict[node[1]].append(node)
 
-        # find the node the left and bottom
+        # sort every column list
+        for c in columns:
+            column_dict[c] = sorted(column_dict[c], key=lambda ele:ele[0], reverse=True)
+
+        # find the target node
         if command_index:
             target = command_index
         else:
             left_column = index_list[0][1]
             left_list = column_dict[left_column]
-            left_list = sorted(left_list, key=lambda ele:ele[0], reverse=True)
             target = left_list[0]
-        column[]
+
+        column_dict[target[1]].remove(target)
+
         # update
         self.map[target] += 1
+        for c in columns:
+            if len(column_dict[c]) == 0:
+                continue
+            deepest = column_dict[c][0][0]
+            for i in reversed(range(deepest+1)):
+                if i-len(column_dict[c]) < 0:
+                    self.map[i, c] = np.random.choice(self.sample_pool)
+                else:
+                    new_node = (i-len(column_dict[c]), c)
+                    self.map[i, c] = self.map[new_node]
+
+
+    def feedempty(self, column_dict):
+        for c in column_dict:
+            for node in column_dict[c]:
+                self.map[node] = np.random.choice(self.sample_pool)
+
 
 
 
 if __name__ == '__main__':
-    env = happy(4, 4, 5, 1234)
+    env = happy(5, 5, 9, 1234)
     env.allsearch()
